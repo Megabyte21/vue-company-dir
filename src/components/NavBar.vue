9f9fbce6aeb70ef5245bb5ac656618f9c92721b9
Megabyte21 <66543643+Megabyte21@users.noreply.github.com>
2023-10-01 21:43:34 -0500

    Redid Some Files

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,43 +1,61 @@
 <script setup>
-import { ref } from "vue";
-const brand = ref('🏢 Fake Company Directory')
+  import { ref } from 'vue'
+  import { useAuth } from '@/composables/useAuth'
+
+  const { isAuthenticated, logout, user } = useAuth()
+
+  const brand = ref(import.meta.env.VITE_APP_NAME)
 </script>
 
-
 <template>
-    <nav>
-        <div class="wrapper">
-            <div class="brand">
-                <span class="brand-title">{{ brand }}</span>
-            </div>
-            <div class="menu">
-                <a href="#" class="menu-item">Departments</a>
-                <a href="#" class="menu-item">Settings</a>
-                <a href="#" class="menu-login">Logout</a>
-            </div>
+  <nav>
+    <div class="wrapper">
+      <RouterLink :to="{ name: 'Home' }" class="brand">
+        <span class="brand-title">{{ brand }}</span>
+      </RouterLink>
+      <div class="menu">
+        <p v-show="isAuthenticated" class="px-2 py-4">
+          Welcome back
+          <strong
+            ><i>{{ user?.email }}</i></strong>
+        </p>
+        <div v-if="isAuthenticated">
+          <RouterLink :to="{ name: 'Settings' }" href="#" class="menu-item">Settings</RouterLink>
+          <button class="menu-logout" @click="logout">Logout</button>
         </div>
-    </nav>
+        <div v-else>
+          <RouterLink :to="{ name: 'Login' }" href="#" class="menu-login">Login</RouterLink>
+        </div>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <style scoped lang="postcss">
-    nav {
-    @apply h-20 bg-slate-900 text-slate-200 flex items-center justify-between;
-    }
+  nav {
+    @apply flex h-20 bg-slate-900 text-slate-200;
     .wrapper {
-        @apply container flex items-center justify-between mx-auto w-full;
-    }
-    .brand {
+      @apply container mx-auto flex w-full items-center justify-between;
+      .brand {
         &-title {
-            @apply text-2xl font-bold text-yellow-500;
+          @apply text-2xl font-bold text-yellow-500;
         }
-    }
-    .menu{
+      }
+      .menu {
         @apply flex gap-2;
+        & div {
+          @apply py-2;
+        }
         &-item {
-            @apply rounded-md px-4 py-2 hover:bg-yellow-500 hover:text-slate-900;
+          @apply rounded-md px-4 py-2 hover:bg-yellow-500 hover:text-slate-900;
         }
         &-login {
-            @apply rounded-md bg-red-500 px-4 py-2 text-red-100 hover:bg-red-700;
+          @apply rounded-md bg-green-500 px-4 py-2 text-red-100 hover:bg-green-700;
         }
+        &-logout {
+          @apply mx-2 rounded-md bg-red-500 px-4 py-2 text-red-100 hover:bg-red-700;
+        }
+      }
     }
+  }
 </style>
